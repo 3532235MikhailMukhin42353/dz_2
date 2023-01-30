79e233c905b2e9db3fef3b8f9ed7f8b63c918393
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-01-30 18:13:14 +0300

    fdf :wq

---
+++ zad_15/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+Console.WriteLine("Введите день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 5) Console.WriteLine("Рабочий день недели");
+else if (number >= 6 && number <= 7)Console.WriteLine("Выходной");
+else Console.WriteLine ("Неккоректная дата!");
+}
+catch
+{
+    Console.WriteLine ("Введите именно число!!!");
+}
