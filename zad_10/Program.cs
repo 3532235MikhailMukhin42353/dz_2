18e1934fbaf702623f9a79f75e8ddcf31f50603e
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-01-30 18:26:21 +0300

    sdfsd :wq

--- zad_10/Program.cs
+++ zad_10/Program.cs
@@ -1,10 +1,12 @@
-﻿try
+﻿
+
+try
 {
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int numA = number / 10;  //123->12
-int numB = numA % 10;   //123->23
+int numA = number / 10;  
+int numB = numA % 10;   
 if (number > 99 && number < 1000) Console.WriteLine($"Число трехзначное, второе число: {numB}");
 else Console.WriteLine("Число не трехзначное!");
 }
--- zad_15/Program.cs
+++ zad_15/Program.cs
@@ -1,13 +1,12 @@
 ﻿try
 {
-Console.WriteLine("Введите день недели: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if (number >= 1 && number <= 5) Console.WriteLine("Рабочий день недели");
-else if (number >= 6 && number <= 7)Console.WriteLine("Выходной");
-else Console.WriteLine ("Неккоректная дата!");
+    Console.WriteLine("Введите день недели: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number >= 1 && number <= 5) Console.WriteLine("Рабочий день недели");
+    else if (number >= 6 && number <= 7) Console.WriteLine("Выходной");
+    else Console.WriteLine("Неккоректная дата!");
 }
 catch
 {
-    Console.WriteLine ("Введите именно число!!!");
+    Console.WriteLine("Введите именно число!!!");
 }
