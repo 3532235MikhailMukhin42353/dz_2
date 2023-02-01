263be2bd3269f224d0bac3958b29867c82953457
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-02-01 12:33:18 +0300

    dsfsdf :wq

---
+++ zad_13/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int a = number.Length;
+if(a > 2) Console.WriteLine($"Третье значение числа равно = {number[2]}");
+else Console.WriteLine("Число не трехзначное!");
+
